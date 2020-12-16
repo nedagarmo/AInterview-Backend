0af1017c9d3457e758c8d4ea7079ff6a2be45fc7
Nelson Garzón <nedagarmo@gmail.com>
2020-12-15 20:02:32 -0500

    feat: server e implementación de typescripts

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import {Request, Response} from "express";
+import express from 'express';
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+import * as jwt from 'jsonwebtoken';
+import * as fs from "fs";
+
+const app: Application = express();
+const port: integer = 3000;
+
+app.use(bodyParser.json());
+
+app.route('/api/login')
+    .post(loginRoute);
+
+const RSA_PRIVATE_KEY = fs.readFileSync('./demos/private.key');
+
+export function loginRoute(req: Request, res: Response) {
+
+    const email = req.body.email,
+          password = req.body.password;
+
+    if (validateEmailAndPassword()) {
+       const userId = findUserIdForEmail(email);
+
+        const jwtBearerToken = jwt.sign({}, RSA_PRIVATE_KEY, {
+                algorithm: 'RS256',
+                expiresIn: 120,
+                subject: userId
+            }
+
+          // send the JWT back to the user
+          // TODO - multiple options available                              
+    }
+    else {
+        // send status 401 Unauthorized
+        res.sendStatus(401); 
+    }
+}
+
+app.listen(port, err => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on ${port}`);
+});
