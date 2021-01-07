7361562f12680c5c6b543c2e5b9d7f8278ec5eb8
Nelson Garzón <nedagarmo@gmail.com>
2021-01-07 05:04:10 -0500

    feat: se agregan nuevas rutas

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,11 @@ const expirationToken:number = 60 * 60 * 24;
 app.use(bodyParser.json());
 app.use(cors());
 app.route('/api/login').post(login);
+app.route('/api/register').post(register);
+app.route('/api/programmer').post(programmer);
+app.route('/api/delete').post(deleteInterview);
+app.route('/api/calendar').get(calendar);
+app.route('/api/interview').post(interview);
 
 const RSA_PRIVATE_KEY = fs.readFileSync(key);
 
@@ -33,8 +38,61 @@ export function login(req: express.Request, res: express.Response) {
     }
 }
 
+export function register(req: express.Request, res: express.Response) {
+    res.status(200).json({ message: "Se ha registrado correctamente" });
+}
+
+export function programmer(req: express.Request, res: express.Response) {
+    res.status(200).json({ message: "Se ha programado la entrevista correctamente" });
+}
+
+export function deleteInterview(req: express.Request, res: express.Response) {
+    res.status(200).json({ message: "Se ha eliminado la entrevista correctamente" });
+}
+
+export function interview(req: express.Request, res: express.Response) {
+    res.status(200).json({
+        title: 'Entrevista Gerente de Proyectos',
+        start: '2021-01-09 15:00:00',
+        end: '2021-01-09 16:00:00',
+        id: '3',
+        email: 'correo@entrevistado.com'
+      });
+}
+
+export function calendar(req: express.Request, res: express.Response) {
+    res.status(200).json([{
+        title: 'Entrevista Desarrollador de Software',
+        start: '2021-01-06 08:00',
+        end: '2021-01-06 09:00',
+        id: '1'
+      },
+      {
+        title: 'Entrevista Arquitecto de Software',
+        start: '2021-01-07 11:00',
+        end: '2021-01-07 12:00',
+        id: '2'
+      },
+      {
+        title: 'Entrevista Gerente de Proyectos',
+        start: '2021-01-09 15:00',
+        end: '2021-01-09 16:00',
+        id: '3'
+      }]);
+}
+
 function validateUser(user: string, password: string): boolean {
-    return true;
+    if(user == "correo@entrevistador.com" && password == "entrevistador")
+    {
+        return true;
+    }
+
+    if(user == "correo@entrevistado.com" && password == "entrevistado")
+    {
+        return true;
+    }
+
+    return false;
 }
 
 function findUserForEmail(email:string): string {
